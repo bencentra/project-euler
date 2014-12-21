0ff019a2b6d42e0b19378ecdbc5af149bcae51a8
Ben Centra <bcentra@constantcontact.com>
2014-12-21 17:27:20 -0500

    solved problem 5

---
+++ problem-5.js
@@ -0,0 +1,21 @@
+var _ = require("underscore");
+
+var i = 19;
+var found = false;
+var num = false;
+while (!num) {
+    found = true;
+    for (var j = 1; j <= 20; j++) {
+        if (i % j != 0) {
+            found = false;
+            break;
+        }
+    }
+    if (found) {
+        num = i;
+    }
+    else {
+        i += 19;
+    }
+}
+console.log(num);
